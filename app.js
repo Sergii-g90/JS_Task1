5752e546bc148834094d80a7da74e2b2c0758650
Sergii-g90 <33447265+Sergii-g90@users.noreply.github.com>
2018-11-28 14:29:26 +0200

    update of JS, making all it works

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
-//Define UI Vars
-const form = document.querySelector('#task-form');
-const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
-const filter = document.querySelector('#filter');
-const taskInput = document.querySelector('#task');
-let test = 'text';
-console.log((test));
+// Define UI Vars
+const form = document.querySelector('#task-form')
+const taskList = document.querySelector('.collection')
+const clearBtn = document.querySelector('.clear-tasks')
+const filter = document.querySelector('#filter')
+const taskInput = document.querySelector('#task')
+let test = 'text'
+console.log((test))
 
-//Load all event listeners
-loadEventListeners();
+// Load all event listeners
+loadEventListeners()
 
-//Load all event listeners
-function loadEventListeners() {
-  //Add task event
-  form.addEventListener('submit', addTask);
+// Load all event listeners
+function loadEventListeners () {
+  // Add task event
+  form.addEventListener('submit', addTask)
 }
 
-//Add tasks
-function addTask(e) {
+// Add tasks
+function addTask (e) {
   if (taskInput.value === '') {
-    alert('Add a task');
+    alert('Add a task')
   }
-  //Create li element
-  const li = document.createElement('li');
-  li.className = 'collection-item';
-  //create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  //create new link element
-  const link = document.createElement('a')
-  //Add class
-  link.className = 'delete-item secondary-content';
-  //Add icon html
-  link.innerHTML = '<i class="fa fa-remove"></i>';
-  //Append the link to li
-  li.appendChild(link);
-  //Append li to ul
-  taskList.appendChild(li);
+  else {
+    //Create li element
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link element
+    const link = document.createElement('a')
+    //Add class
+    link.className = 'delete-item secondary-content';
+    //Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append the link to li
+    li.appendChild(link);
+    //Append li to ul
+    taskList.appendChild(li);
 
-  //clear input
-  taskInput.value = '';
-
+    //clear input
+    taskInput.value = '';
+  }
   e.preventDefault();
 }
