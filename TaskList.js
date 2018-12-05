5adb702b4ef07ab8b0147d8acac175c1da989d67
Sergii-g90 <33447265+Sergii-g90@users.noreply.github.com>
2018-12-05 12:03:09 +0200

    Added functions for removing item, clearing list, filtering items in the task lists

--- TaskList.js
+++ TaskList.js
@@ -12,6 +12,23 @@ loadEventListeners();
 function loadEventListeners () {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task addEventListener
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
+}
+
+// Filter Tasks
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+       task.style.display = 'block';
+    } else {
+       task.style.display = 'none';
+    }
+  });
 }
 
 // Add tasks
@@ -40,4 +57,20 @@ function addTask (e) {
     taskInput.value = '';
   }
   e.preventDefault();
+}
+
+// Remove tasks
+function removeTask (e) {
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks(e) {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 }
